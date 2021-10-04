7abcd7bb324b18142052d8e925cc66a0357a5e6a
Gellért <palfigell9@gmail.com>
2021-10-04 21:09:38 +0200

    finalised health endpoint, cleaned up main and outsourced some methods for cleaner code

--- main.go
+++ main.go
@@ -1,31 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	//"github.com/go-chi/chi/v5/middleware"
 )
 
+const (
+	api = "a7886d54f2fbe455ffb285d182c8e2db"
+)
+
+var (
+	r = chi.NewRouter()
+	url = "api.openweathermap.org/data/2.5/weather?q=London&appid=" + api
+)
+
+type Health struct {
+	Health string `json:"health"`
+}
+
+func checkHealth(w http.ResponseWriter, r *http.Request){
+	healtStatus := Health{Health: "healthy"}
+	resp, err := http.Get("https://" + url)
+
+	fmt.Println(url)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println("checkhealth endpoint hit")
+	json.NewEncoder(w).Encode(healtStatus)
+}
+
+func handleRequests(){
+	port := ":3000"
+
+	r.HandleFunc("/api/health", checkHealth)
+	fmt.Println("Server is up and running on port " + port)
+	log.Fatal(http.ListenAndServe(port, r))
+}
 
 func main() {
-	//apikey = "a7886d54f2fbe455ffb285d182c8e2db"
-	//city := "Budapest"
-	port := ":3000"
-	r := chi.NewRouter()
-	//toDo dont hardcode api key
-	//url := "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a7886d54f2fbe455ffb285d182c8e2db"
 
-	// Get api health
-	r.Get("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("healt"))
-	})
-
-	r.Get("/api/weather", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/api/wesather", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("weather"))
 	})
-	//toDo write something with actual information
-	fmt.Println("somethingplaceholer")
-	http.ListenAndServe(port, r)
+
+	handleRequests()
 }
