a270271243659559ef52d77178c4066a58078928
Gellért <palfigell9@gmail.com>
2021-10-05 13:32:08 +0200

    weather endpoint now prints out the api response

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -23,11 +24,20 @@ type Health struct {
 	Health string `json:"health"`
 }
 
+type Weather struct {
+	Temp      float32 `json:"temp"`
+	WindSpeed float32 `json:"wind_speed"`
+	Sunrise   string `json:"sunrise"`
+	Sunset    string `json:"sunset"`
+	Pressure  int `json:"pressure"`
+	Humidity  int `json:"humidity"`
+}
+
 func checkHealth(w http.ResponseWriter, r *http.Request){
-	healtStatus := Health{Health: "healthy"}
+	healthStatus := Health{Health: "healthy"}
 	resp, err := http.Get("https://" + url)
 
-	fmt.Println(url)
+	//fmt.Println(url)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -36,23 +46,37 @@ func checkHealth(w http.ResponseWriter, r *http.Request){
 
 	defer resp.Body.Close()
 
-	fmt.Println("checkhealth endpoint hit")
-	json.NewEncoder(w).Encode(healtStatus)
+	if resp.StatusCode == 200 {
+		fmt.Println("checkhealth endpoint hit")
+		json.NewEncoder(w).Encode(healthStatus)
+	}
+
+}
+
+func fetchWeather(w http.ResponseWriter, r *http.Request){
+	//toDo make the api call only once
+	resp, err  := http.Get("https://" + url)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//weather := Weather{resp}
+	body, err := ioutil.ReadAll(resp.Body)
+	w.Write(body)
+	fmt.Println(string(body))
+
 }
 
 func handleRequests(){
 	port := ":3000"
 
 	r.HandleFunc("/api/health", checkHealth)
+	r.HandleFunc("/api/weather", fetchWeather)
 	fmt.Println("Server is up and running on port " + port)
 	log.Fatal(http.ListenAndServe(port, r))
 }
 
 func main() {
-
-	r.Get("/api/wesather", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("weather"))
-	})
-
 	handleRequests()
 }
